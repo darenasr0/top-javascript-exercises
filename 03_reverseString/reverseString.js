c2aa01c1c72cb055feada0550b34a39be9e2799f
darenasr0 <151607295+darenasr0@users.noreply.github.com>
2024-01-17 18:53:27 +0100

    resolved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let separar=string.split("");
+    let invertir=separar.reverse();
+    let unir=invertir.join("");
+    
+    return unir;
 };
 
 // Do not edit below this line
